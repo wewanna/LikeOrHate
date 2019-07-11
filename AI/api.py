931727a1c3aafab7b7bd96bf447eb687a2fdfc1e
Cheolwan-Park <cheolwan.park552@gmail.com>
2019-07-11 13:36:24 +0900

    .

--- AI/api.py
+++ AI/api.py
@@ -1,18 +1,26 @@
 from flask import Flask, request
-from AI.run import predict
+from flask_cors import CORS
+import run
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/', methods=['POST'])
 def upload_train():
     data = request.get_json()
     print(data['text'])
-    result = predict(str(data['text']))
+    result = run.predict(str(data['text']))
     poornag = ''
     if result == 0:
         poornag = '부정적'
     else:
         poornag = '긍정적'
+
+    response = Response()
+    response.headers[
+        'Access-Control-Allow-Headers'] = 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS, HEAD'
+
     return poornag, 200
 
 app.run(port=5000, debug=True, threaded=True)
